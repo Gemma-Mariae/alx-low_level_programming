256aec56897e9fff2afba9bed27644f20b0a7326
Gemma-Mariae <divinechisom1995@gmail.com>
2021-07-19 11:12:59 -0700

    Function-like macros thst adds

---
+++ 0x0D-preprocessor/4-sum.h
@@ -0,0 +1,12 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+/*
+ * File: 4-sum.h
+ * Author: Ukonu Divine Chisom
+ * Desc: Header file that defines a function-like macro SUM(x, y)
+ *       that computes the sum of the numbers x and y.
+ */
+
+#define SUM(x, y) ((x) + (y))
+
+#endif
