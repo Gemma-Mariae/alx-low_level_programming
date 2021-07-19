ee4bd612ff1541e8e9929f4d48675a1c1dd8812e
Gemma-Mariae <divinechisom1995@gmail.com>
2021-07-19 10:18:49 -0700

    A header file that defines a macro named PI

---
+++ 0x0D-preprocessor/1-pi.h
@@ -0,0 +1,13 @@
+#ifndef HOLBERTON_H 
+#define HOLBERTON_H
+
+/*
+ * File: 1-pi.h
+ * Author: Ukonu Divine Chisom
+ * Desc: Header file that defines a macro named PI
+ *       as an abbreviation for the token 3.14159265359.
+ */
+
+#define PI 3.14159265359
+
+#endif
